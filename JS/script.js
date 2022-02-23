4a8560476be8a28b7e9b7749aa5ca2e732747296
indhu-github <indhusworkmail@gmail.com>
2022-02-23 16:53:35 +0530

    ready

--- JS/script.js
+++ JS/script.js
@@ -1,23 +1,22 @@
-window.onload=()=>{
-    getAdvice();
-}
+window.onload = () => {
+  getAdvice();
+};
 
-const button = document.querySelector('.advice_btn');
-const adviceText = document.querySelector('.advice_text');
-const adviceNumber = document.querySelector('.advice_number span');
+const button = document.querySelector(".advice_btn");
+const adviceText = document.querySelector(".advice_text");
+const adviceNumber = document.querySelector(".advice_number span");
 
-button.addEventListener('click',getAdvice);
+button.addEventListener("click", getAdvice);
 
-async function getAdvice(){
-    try{
-         response = await fetch('https://api.adviceslip.com/advice');
-         data = await response.json();
-        id= data.slip.id;
-        advice=data.slip.advice;
-        adviceNumber.innerText=id;
-        adviceText.innerText=advice; 
-    }
-    catch(error){
-        console.log(error);
-    }
+async function getAdvice() {
+  try {
+    response = await fetch("https://api.adviceslip.com/advice");
+    data = await response.json();
+    id = data.slip.id;
+    advice = data.slip.advice;
+    adviceNumber.innerText = id;
+    adviceText.innerText = advice;
+  } catch (error) {
+    console.log(error);
+  }
 }
